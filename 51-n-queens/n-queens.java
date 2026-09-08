da33a8b42b2c2c8d0003c2242039c1c888d477e6
Gagan Yadav <ygagan012@gmail.com>
2026-09-08 21:23:09 +0530

    Time: 2 ms (81.41%) | Memory: 46.8 MB (28.37%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,81 +1,73 @@
 class Solution {
-    static boolean isSafeToo(int rowIndex , int colIndex , int n ,char[][] board){
-    
-        //now we will check directions
-        //for left direction
+    static boolean issafetoo(int rowIndex , int colIndex , int n , char[][]board){
+        
+        //for left directions
         int row = rowIndex;
         int col = colIndex;
-
-        while(col >= 0){
+        while(col>=0){
             if(board[row][col] == 'Q'){
                 return false;
             }
             col--;
         }
 
-        //for left upper diagonal
+        //for left upper diagonals
         row = rowIndex;
         col = colIndex;
-        while(row>=0 && col >= 0){
+        while(row>=0 && col >=0){
             if(board[row][col] == 'Q'){
                 return false;
             }
-            col = col -1;
-            row = row -1;
+            row--;
+            col--;
         }
 
-        //for left lower diagonal
+        //for left down diagonals
         row = rowIndex;
         col = colIndex;
-        while(row < n && col >= 0){
+        while(row<n && col >=0){
             if(board[row][col] == 'Q'){
                 return false;
             }
-            row = row +1;
-            col = col -1;
+            row++;
+            col--;
         }
         return true;
     }
-    static void solve(int n,char [][] board ,List<List<String>> output,int colIndex){
+    static void solve(int n , char[][]board ,int colIndex ,List<List<String>> output){
         //base case
-        //agar hame koi valid arrangment milgyi hai toh return kardo 
-        //lekin return karne se pehle use store kardo
-        if(colIndex>=n){
-
-            //ye loop saari values ko string me laaarha h taaki list me add karsake
-            List<String>temp = new ArrayList<>();
+        //yaani valid arrangement mil chuka hai toh 
+        if(colIndex >= n){
+            List<String> temp = new ArrayList<>();
             for(int i=0 ; i<n ; i++){
                 temp.add(new String(board[i]));
             }
-
             output.add(temp);
             return;
         }
 
-        //1 case hum karege baaki recursion
-        for(int rowIndex=0 ; rowIndex<n ; rowIndex++){
-            if(isSafeToo(rowIndex,colIndex,n,board)){
+        //1 case hum baaki recursion
+        for(int rowIndex= 0 ; rowIndex<n ; rowIndex++){
+            if(issafetoo(rowIndex,colIndex,n,board)){
+
                 board[rowIndex][colIndex] = 'Q';
 
-                //baaki recursion ko dedo
-                solve(n,board,output,colIndex+1);
+                solve(n,board,colIndex+1,output);
 
-                //backtracking or undo step
+                //backtrack
                 board[rowIndex][colIndex] = '.';
             }
         }
-    }  
+    }
     public List<List<String>> solveNQueens(int n) {
-        //ek nxn ka board banaya hai jaha hamara queen bharegi 
+        //sabse pehle hum board baanayege phir usme . put karege
         char [][] board = new char [n][n];
-        List<List<String>> output = new ArrayList<>();
-        
-        //ab hum pure board ko dot se bhardega
         for(int i=0 ; i<n ; i++){
             Arrays.fill(board[i],'.');
         }
         int colIndex = 0;
-        solve(n,board,output,colIndex);
+        List<List<String>> output = new ArrayList<>();
+        solve(n,board,colIndex,output);
         return output;
     }
 }
